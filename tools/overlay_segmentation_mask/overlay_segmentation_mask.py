2022270040df26ddcc6f846537c08ec78631a1a8
znerp <virtual_flame@yahoo.de>
2018-12-29 17:24:56 +0100

    small code changes

--- tools/overlay_segmentation_mask/overlay_segmentation_mask.py
+++ tools/overlay_segmentation_mask/overlay_segmentation_mask.py
@@ -14,14 +14,13 @@ import skimage.measure
 parser = argparse.ArgumentParser()
 parser.add_argument('input_file', type=argparse.FileType('r'), help='input file')
 parser.add_argument('mask_file', type=argparse.FileType('r'), help='mask file')
-parser.add_argument('out_file', type=argparse.FileType('w'), help='out file (PNG)')
+parser.add_argument('out_file', type=str, help='out file (PNG)') # file would be created immediately with argparse.FileType('w') s.t. file cannot be renamed on galaxy
 parser.add_argument('--grey', dest='greyscale', action='store_true', help='image is greyscale')
 parser.add_argument('--label', dest='label', action='store_true', help='plot label')
+parser.add_argument('--label_color', dest='label_color', default='#FFFF00', help='label color')
 parser.add_argument('--thickness', dest='thickness', default=0.3, type=float, help='thickness')
 parser.add_argument('--stroke_color', dest='stroke_color', default='#ff0000', help='stroke color')
-parser.add_argument('--label_color', dest='label_color', default='#FFFF00', help='label color')
 args = parser.parse_args()
-
 img = skimage.io.imread(args.input_file.name)
 label = skimage.io.imread(args.mask_file.name)
 
@@ -40,4 +39,5 @@ else:
 plt.contour(label, linewidths=args.thickness, colors=args.stroke_color)
 
 fig.canvas.print_png("tmp.png")
-os.rename("tmp.png", args.out_file.name)
+os.rename("tmp.png", args.out_file)
+# png_name = args.out_file.replace('.dat', '.png')
