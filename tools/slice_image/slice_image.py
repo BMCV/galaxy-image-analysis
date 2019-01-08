8c9304e37255ad3cc4f827871953d4f88321e6e4
znerp <virtual_flame@yahoo.de>
2019-01-08 09:11:36 +0100

    ignore FutureWarnings

--- tools/slice_image/slice_image.py
+++ tools/slice_image/slice_image.py
@@ -9,24 +9,29 @@ import skimage.util
 import skimage.feature
 from scipy.stats import entropy as scipy_entropy
 
-def slice_image(input_file, out_folder, label=None, label_out_folder=None, window_size=64, stride=1, bg_thresh=1, limit_slices=False, n_thresh=5000):
+def slice_image(input_file, out_folder, label=None, label_out_folder=None, window_size=64, 
+                stride=1, bg_thresh=1, limit_slices=False, n_thresh=5000, seed=None):
     #TODO NOT Implemented:process labels
+    # --> label and label_out_folder useless so far
+
+    # primarily for testing purposes:
+    if seed is not None:
+        random.seed(seed)
 
     img_raw = skimage.io.imread(input_file)
     if len(img_raw.shape) == 2:
         img_raw = np.expand_dims(img_raw, 3)
 
-    patches_raw = skimage.util.view_as_windows(img_raw, (window_size, window_size, img_raw.shape[2]), step=stride)
-    patches_raw = patches_raw.reshape([-1, window_size, window_size, img_raw.shape[2]])
-
-    filename = os.path.splitext(os.path.basename(input_file))[0]
-    new_path = out_folder+"/"+filename+"_%d.tiff"
-    
-    #samples for thresholding the amount of slices
-    sample = random.sample(range(patches_raw.shape[0]), n_thresh)
-
-    with warnings.catch_warnings():
+    with warnings.catch_warnings(): # ignore FutureWarning
         warnings.simplefilter("ignore")
+        patches_raw = skimage.util.view_as_windows(img_raw, (window_size, window_size, img_raw.shape[2]), step=stride)
+        patches_raw = patches_raw.reshape([-1, window_size, window_size, img_raw.shape[2]])
+
+        filename = os.path.splitext(os.path.basename(input_file))[0]
+        new_path = out_folder+"/"+filename+"_%d.tiff" # does the backslash work for every os?
+        
+        #samples for thresholding the amount of slices
+        sample = random.sample(range(patches_raw.shape[0]), n_thresh)
 
         for i in range(0, patches_raw.shape[0]):
             # TODO improve
@@ -61,8 +66,10 @@ if __name__ == "__main__":
     parser.add_argument('--bg_thresh', dest='bg_thresh', type=float, default=0, help='skip patches without information using a treshold')
     parser.add_argument('--limit_slices', dest='limit_slices', type=bool, default=False, help='limit amount of slices')
     parser.add_argument('--n_thresh', dest='n_thresh', type=int, default=5000, help='amount of slices')
+    parser.add_argument('--seed', dest='seed', type=int, default=None, help='seed for random choice of limited slices')
     args = parser.parse_args()
 
     slice_image(args.input_file.name, args.out_folder,
                 label=args.label_file, label_out_folder=args.label_out_folder,
-                stride=args.stride, window_size=args.window_size, bg_thresh=args.bg_thresh, limit_slices=args.limit_slices, n_thresh=args.n_thresh)
+                stride=args.stride, window_size=args.window_size, bg_thresh=args.bg_thresh, 
+                limit_slices=args.limit_slices, n_thresh=args.n_thresh, seed=args.seed)
