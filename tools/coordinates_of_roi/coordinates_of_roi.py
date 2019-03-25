a08f38c3ab2e4b551718939daff2a9923ba8dc59
znerp <virtual_flame@yahoo.de>
2019-03-25 13:53:29 +0100

    remove offset

--- tools/coordinates_of_roi/coordinates_of_roi.py
+++ tools/coordinates_of_roi/coordinates_of_roi.py
@@ -14,7 +14,7 @@ def get_pixel_values(im, pixel_table, white_obj, threshold, offset=[0,0]):
     img_width = data.shape[1]
     for j in range(img_width):
         for i in range(img_height):
-            if white_obj == False:
+            if white_obj == False: 
                 if data[i,j] <= threshold:
                     x.append(i + offset[0])
                     y.append(j + offset[1])
@@ -31,12 +31,10 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(description = "Create a csv table with Coordinates of the ROI")
     parser.add_argument("im", help = "Paste path to out.png (output created by transformation)")
     parser.add_argument("pixel_table", help = "Paste path to file in which list with all pixles > threshold should be saved")
-    parser.add_argument('offset_x', type=int, help='offset in x direction (width)', default=0)
-    parser.add_argument('offset_y', type=int, help='offset in y direction (height)', default=0)
     parser.add_argument("--white_obj", dest = "white_obj", default=False, help = "If set objects in image are white otherwise black", action = "store_true")
     parser.add_argument("--threshold", dest = "threshold", default = 0.5, help = "Enter desired threshold value", type = float)
     
     args = parser.parse_args()  
     # with warnings.catch_warnings():
 	#     warnings.simplefilter("ignore") 
-    get_pixel_values(args.im, args.pixel_table, args.white_obj, args.threshold, [args.offset_x, args.offset_y])     
+    get_pixel_values(args.im, args.pixel_table, args.white_obj, args.threshold)     
