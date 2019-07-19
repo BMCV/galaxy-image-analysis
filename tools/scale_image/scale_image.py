d3a8358f2c3851af42e0f6370723e02affbd6c7a
Thomas Wollmann <tom.wollmann@ish.de>
2019-07-19 16:08:35 +0200

    added missing dependency

--- tools/scale_image/scale_image.py
+++ tools/scale_image/scale_image.py
@@ -3,33 +3,27 @@ import sys
 import skimage.io
 import skimage.transform
 import scipy.misc
-import warnings
-import os
 from PIL import Image
 
  
 def scale_image(input_file, output_file, scale, order=1):
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        Image.MAX_IMAGE_PIXELS = 50000*50000
-        img_in = skimage.io.imread(input_file)
-        if order == 0:
-            interp = 'nearest'
-        elif order == 1:
-            interp = 'bilinear'
-        elif order == 2:
-            interp = 'bicubic'
-
-        if ',' in scale:
-            scale = scale[1:-1].split(',')
-            scale = [int(i) for i in scale]
-        elif '.' in scale:
-            scale = float(scale)
-        else:
-            scale = int(scale)
-
-        res = scipy.misc.imresize(img_in, scale, interp=interp)
-        skimage.io.imsave(output_file, res)
+    Image.MAX_IMAGE_PIXELS = 50000*50000
+    img_in = skimage.io.imread(input_file)
+    if order == 0:
+        interp = 'nearest'
+    elif order == 1:
+        interp = 'bilinear'
+    elif order == 2:
+        interp = 'bicubic'
+    if ',' in scale:
+        scale = scale[1:-1].split(',')
+        scale = [int(i) for i in scale]
+    elif '.' in scale:
+        scale = float(scale)
+    else:
+        scale = int(scale)
+    res = scipy.misc.imresize(img_in, scale, interp=interp)
+    skimage.io.imsave(output_file, res)
 
 
 if __name__ == "__main__":
