dffb3b810bad82bc3fcc6b46a4be67f79fb4735d
Qi Gao <q.gao@hotmail.com>
2022-01-14 05:11:05 +0100

    1, updated tool "image_registration_affine": more options of the image similarity metric; support of RGB(A) images
    2, added new tool "3d_tensor_feature_dimension_reduction"

---
+++ tools/3d_tensor_feature_dimension_reduction/3d_tensor_feature_dimension_reduction.py
@@ -0,0 +1,38 @@
+"""
+Copyright 2022 Biomedical Computer Vision Group, Heidelberg University.
+
+Distributed under the MIT license.
+See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+
+"""
+
+import argparse
+import warnings
+
+import numpy as np
+import tifffile
+import umap
+
+
+def feature_dimension_reduction(tensor_fn, tiff_fn, nCh=5):
+    npobj = np.load(tensor_fn)
+    ts = npobj[npobj.files[0]]
+
+    assert len(ts.shape) == 3 and ts.shape[-1] > nCh, \
+        'the input tensor data must be three-dimensional'
+
+    embedding = umap.UMAP(n_components=nCh).fit_transform(np.reshape(ts, (-1, ts.shape[-1])))
+    img = np.reshape(embedding, (ts.shape[0], ts.shape[1], -1)).astype(np.float32)
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        tifffile.imwrite(tiff_fn + '.tif', np.transpose(img, (2, 0, 1)), imagej=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Dimensionality reduction for features (channels) of 3D tensor using UMAP")
+    parser.add_argument("tensor_fn", help="Path to the 3D tensor data")
+    parser.add_argument("nCh", type=int, help="The reduced dimension of features")
+    parser.add_argument("tiff_fn", help="Path to the output file")
+    args = parser.parse_args()
+    feature_dimension_reduction(args.tensor_fn, args.tiff_fn, args.nCh)
