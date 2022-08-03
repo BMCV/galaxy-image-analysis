6d6a6fd04fa1d1f9a5bf1aff451e43be072c0293
Qi Gao <64555893+qgao-hd@users.noreply.github.com>
2022-08-03 18:37:54 +0200

    added citation and moved deprecated tools (#47)
    * added citation and moved deprecated tools
    * updated copyright info

--- tools/landmark_registration/landmark_registration.py
+++ tools/landmark_registration/landmark_registration.py
@@ -1,5 +1,5 @@
 """
-Copyright 2017-2022 Biomedical Computer Vision Group, Heidelberg University.
+Copyright 2017-2022 Biomedical Computer Vision Group & Core Facility Platform Mannheim, Heidelberg University.
 
 Distributed under the MIT license.
 See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
