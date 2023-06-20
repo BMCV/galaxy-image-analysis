f71726a83166f3c62d357d9a547524d1b7fc9d7d
Leonid Kostrykin <void@evoid.de>
2023-06-20 20:27:30 +0200

    fixes superdsm tool so that test passes

--- tools/superdsm/run-superdsm.py
+++ tools/superdsm/run-superdsm.py
@@ -7,27 +7,54 @@ See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
 """
 
 import argparse
-import json
+import imghdr
 import pathlib
 import shutil
-import subprocess
 import tempfile
 
+import ray
+import superdsm.automation
+import superdsm.io
+import superdsm.render
 
-def create_task(image_filepath, num_cpus, num_threads):
-    return {
-        'runnable': True,
-        'num_cpus': int(num_cpus),
-        'environ': {
-            'MKL_NUM_THREADS': int(num_threads),
-            'OPENBLAS_NUM_THREADS': int(num_threads),
-        },
-        'img_pathpattern': str(image_filepath),
-        'seg_pathpattern': 'masks.png',
-        'cfg_pathpattern': 'cfg.json',
-        'overlay_pathpattern': 'overlay.png',
-        'file_ids': [[]],
-    }
+
+# It was not possible to get ray to work in conjunction with planemo, though the reason remains
+# unclear. Most of the time, the attempts failed due to out-of-memory issues, although ray
+# shouldn't consume any significant amount of memory. Thus, below, ray is effectively "patched
+# out" of SuperDSM. This is neither efficient nor clean, but at least it works.
+
+
+class _Future:
+    def __init__(self, getter):
+        self.getter = getter
+
+
+class _ray_remote:
+    def __init__(self, func):
+        self.func = func
+
+    def remote(self, *args, **kwargs):
+        args = [(arg.getter() if isinstance(arg, _Future) else arg) for arg in args]
+        kwargs = {key: (arg.getter() if isinstance(arg, _Future) else arg) for key, arg in kwargs.items()}
+        return _Future(lambda: self.func(*args, **kwargs))
+
+
+def _ray_wait(futures, num_returns):
+    return futures[:num_returns], futures[num_returns:]
+
+
+def _ray_put(obj):
+    return _Future(lambda: obj)
+
+
+def _ray_get(future):
+    return future.getter()
+
+
+ray.remote = _ray_remote
+ray.wait = _ray_wait
+ray.put = _ray_put
+ray.get = _ray_get
 
 
 if __name__ == "__main__":
@@ -36,16 +63,26 @@ if __name__ == "__main__":
     parser.add_argument('cfg', help='Path to the file containing the configuration')
     parser.add_argument('masks', help='Path to the file containing the segmentation masks')
     parser.add_argument('overlay', help='Path to the file containing the overlay of the segmentation results')
-    parser.add_argument('num_cpus', type=int)
-    parser.add_argument('num_threads', type=int)
+    parser.add_argument('seg_border', type=int)
     args = parser.parse_args()
 
     with tempfile.TemporaryDirectory() as tmpdirname:
         tmpdir = pathlib.Path(tmpdirname)
-        task = create_task(num_cpus=args.num_cpus, num_threads=args.num_threads, image_filepath=args.image)
-        with open(str(tmpdir / 'task.json'), 'w') as fp:
-            json.dump(task, fp)
-        subprocess.run(['python', '-m', 'superdsm.batch', tmpdirname, '--run'], check=True)
-        shutil.copy(str(tmpdir / 'masks.png'), args.masks)
-        shutil.copy(str(tmpdir / 'cfg.json'), args.cfg)
-        shutil.copy(str(tmpdir / 'overlay.png'), args.overlay)
+        img_ext = imghdr.what(args.image)
+        img_filepath = tmpdir / f'input.{img_ext}'
+        shutil.copy(str(args.image), img_filepath)
+
+        ray.init(local_mode=True, log_to_driver=True)
+        pipeline = superdsm.pipeline.create_default_pipeline()
+        cfg = superdsm.config.Config()
+        img = superdsm.io.imread(img_filepath)
+        data, cfg, _ = superdsm.automation.process_image(pipeline, cfg, img)
+
+        with open(args.cfg, 'w') as fp:
+            cfg.dump_json(fp)
+
+        overlay = superdsm.render.render_result_over_image(data, border_width=args.seg_border, normalize_img=False)
+        superdsm.io.imwrite(args.overlay, overlay)
+
+        masks = superdsm.render.rasterize_labels(data)
+        superdsm.io.imwrite(args.masks, masks)
