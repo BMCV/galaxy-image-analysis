7d8585b4e36271d4dae536eb939b0e89a0edb605
Leonid Kostrykin <void@evoid.de>
2023-06-22 22:09:43 +0200

    reduces code

--- tools/ray/test-ray.py
+++ tools/ray/test-ray.py
@@ -10,4 +10,12 @@ if __name__ == "__main__":
     pipeline = superdsm.pipeline.create_default_pipeline()
     cfg = superdsm.config.Config()
     img = skimage.io.imread(sys.argv[1])
-    data, cfg, _ = superdsm.automation.process_image(pipeline, cfg, img)
+
+    superdsm.automation._estimate_scale(img, num_radii=10, thresholds=[0.01])
+    cfg['AF_scale'] = None
+    cfg['c2f-region-analysis/AF_min_atom_radius'] = 0.33
+    cfg['c2f-region-analysismin_atom_radius/'] = 30
+
+    from superdsm.c2freganal import C2F_RegionAnalysis
+    stage = C2F_RegionAnalysis()
+    stage.process(dict(g_raw = img), cfg, out = superdsm.output.get_output(), log_root_dir = None)
