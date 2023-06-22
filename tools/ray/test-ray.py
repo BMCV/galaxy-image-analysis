873ea10de8785314da3603d0b4f42447a681de66
Leonid Kostrykin <void@evoid.de>
2023-06-22 23:11:43 +0200

    reduces code

--- tools/ray/test-ray.py
+++ tools/ray/test-ray.py
@@ -3,7 +3,6 @@ import sys
 import skimage.io
 import ray
 
-import math
 import scipy.ndimage as ndi
 import numpy as np
 
@@ -12,10 +11,7 @@ if __name__ == "__main__":
     ray.init(num_cpus=1, log_to_driver=True)
     img = skimage.io.imread(sys.argv[1])
 
-    sigma_list = np.linspace(20, 200, 10) / math.sqrt(2)
-    sigma_list = np.concatenate([[sigma_list.min() / 2], sigma_list])
-   
-    for sigma in sigma_list:
+    for sigma in np.linspace(100, 200, 5) / np.sqrt(2):
         ndi.gaussian_laplace(img, sigma)
 
     ray.put(img)
