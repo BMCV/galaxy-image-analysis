0207f49fa6214a1d8749ad7e6c9872bb42a6799b
Leonid Kostrykin <void@evoid.de>
2023-06-22 22:53:09 +0200

    reduces code

--- tools/ray/test-ray.py
+++ tools/ray/test-ray.py
@@ -2,12 +2,20 @@ import sys
 
 import skimage.io
 import ray
-import superdsm.automation
+
+import math
+import scipy.ndimage as ndi
+import numpy as np
 
 
 if __name__ == "__main__":
     ray.init(num_cpus=1, log_to_driver=True)
     img = skimage.io.imread(sys.argv[1])
 
-    superdsm.automation._estimate_scale(img, num_radii=10, thresholds=[0.01])
+    sigma_list = np.linspace(20, 200, 10) / math.sqrt(2)
+    sigma_list = np.concatenate([[sigma_list.min() / 2], sigma_list])
+   
+    for sigma in sigma_list:
+        ndi.gaussian_laplace(img, sigma)
+
     ray.put(img)
