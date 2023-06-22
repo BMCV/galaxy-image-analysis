533197b385542c96aa01c029c27f88a80353c045
Leonid Kostrykin <void@evoid.de>
2023-06-22 23:37:15 +0200

    reduces code

--- tools/ray/test-ray.py
+++ tools/ray/test-ray.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     ray.init(num_cpus=1, log_to_driver=True)
     img = np.zeros((1344, 1024))
 
-    for sigma in range(5):
+    for _ in range(3):
         ndi.gaussian_laplace(img, 200)
 
     ray.put(img)
