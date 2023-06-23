0bb5331b4ab252b1ead7710857b8f660b1ec1c09
Leonid Kostrykin <void@evoid.de>
2023-06-23 11:08:10 +0200

    adds support for `GALAXY_SLOTS` in superdsm

--- tools/superdsm/run-superdsm.py
+++ tools/superdsm/run-superdsm.py
@@ -8,6 +8,7 @@ See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
 
 import argparse
 import imghdr
+import os
 import pathlib
 import shutil
 import tempfile
@@ -25,7 +26,21 @@ if __name__ == "__main__":
     parser.add_argument('masks', help='Path to the file containing the segmentation masks')
     parser.add_argument('overlay', help='Path to the file containing the overlay of the segmentation results')
     parser.add_argument('seg_border', type=int)
+    parser.add_argument('slots', type=int)
     args = parser.parse_args()
+
+    if args.slots >= 2:
+        num_threads_per_process = 2
+        num_processes = args.slots // num_threads_per_process
+    else:
+        num_threads_per_process = 1
+        num_processes = 1
+
+    os.environ['MKL_NUM_THREADS'] = str(num_threads_per_process)
+    os.environ['OPENBLAS_NUM_THREADS'] = str(num_threads_per_process)
+    os.environ['MKL_DEBUG_CPU_TYPE'] = '5'
+
+    ray.init(num_cpus=num_processes, log_to_driver=True)
 
     with tempfile.TemporaryDirectory() as tmpdirname:
         tmpdir = pathlib.Path(tmpdirname)
@@ -33,7 +48,6 @@ if __name__ == "__main__":
         img_filepath = tmpdir / f'input.{img_ext}'
         shutil.copy(str(args.image), img_filepath)
 
-        ray.init(num_cpus=1, log_to_driver=True)
         pipeline = superdsm.pipeline.create_default_pipeline()
         cfg = superdsm.config.Config()
         img = superdsm.io.imread(img_filepath)
