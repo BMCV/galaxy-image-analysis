743d5a491adfbdd44d45c622a079218ded26ec43
Leonid Kostrykin <void@evoid.de>
2023-07-08 00:54:17 +0200

    fixes flake8 for count_objects tool

--- tools/count_objects/count_objects.py
+++ tools/count_objects/count_objects.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
 
 import argparse
-import numpy as np
-import os
+
 import skimage.io
-from skimage.measure import regionprops 
+from skimage.measure import regionprops
 
 parser = argparse.ArgumentParser(description='Count Objects')
 parser.add_argument('input_file', type=argparse.FileType('r'),
                     help='Label input file')
 parser.add_argument('output_file', type=argparse.FileType('w'),
                     help='Tabular output file')
-args = parser.parse_args() 
+args = parser.parse_args()
 
 img_raw = skimage.io.imread(args.input_file.name)
 res = len(regionprops(img_raw))
