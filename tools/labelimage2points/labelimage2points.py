8223219489e416dea266ad6fa85e3279e42042ba
Leonid Kostrykin <void@evoid.de>
2023-11-01 13:51:44 +0100

    updates "Convert label map to points (center of masses)" tool

--- tools/labelimage2points/labelimage2points.py
+++ tools/labelimage2points/labelimage2points.py
@@ -1,33 +1,29 @@
 import argparse
-import sys
+
+import numpy as np
 import pandas as pd
 import skimage.io
-from skimage.measure import label
-from skimage.data import checkerboard
-import numpy as np
 
- 
 
 def labelimage2points(input_file):
-    img_in = skimage.io.imread(input_file) 
-        
-    #amount of regions
+    img_in = skimage.io.imread(input_file)
+
+    # amount of regions
     amount_label = np.max(img_in)
-    
+
     # iterate over all regions in order to calc center of mass
     center_mass = []
-    for i in range(1,amount_label+1):    
-        #get coordinates of region
-        coord = np.where(img_in==i)
+    for i in range(1, amount_label + 1):
+        # get coordinates of region
+        coord = np.where(img_in == i)
         # be carefull with x,y coordinates
-        center_mass.append([np.mean(coord[1]),np.mean(coord[0])])
+        center_mass.append([np.mean(coord[1]), np.mean(coord[0])])
 
-    #make data frame of detections
+    # make data frame of detections
     out_dataFrame = pd.DataFrame(center_mass)
 
-
-    #return
-    return(out_dataFrame)
+    # return
+    return out_dataFrame
 
 
 if __name__ == "__main__":
@@ -35,12 +31,12 @@ if __name__ == "__main__":
     parser.add_argument('input_file', help='input file')
     parser.add_argument('out_file', help='out file (CSV)')
 
-    args        = parser.parse_args()
-    input_file  = args.input_file
-    out_file    = args.out_file
+    args = parser.parse_args()
+    input_file = args.input_file
+    out_file = args.out_file
 
-    #TOOL
+    # TOOL
     out_dataFrame = labelimage2points(input_file)
 
-    #Print to csv file
+    # Print to csv file
     out_dataFrame.to_csv(out_file, index=False, header=False, sep="\t")
