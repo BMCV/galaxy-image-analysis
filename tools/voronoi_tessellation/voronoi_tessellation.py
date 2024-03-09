43f036ed5341c36a1e076f9df9f47a3743ef4e6f
Leonid Kostrykin <void@evoid.de>
2024-03-09 23:18:24 +0100

    Add `voronoi_tessellation` tool (#108)
    Squashed commit of the following:
    
    commit 3cf5e31c474d1568bbb57e4c8573af0528351a89
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 19:04:19 2024 +0100
    
        Fix test results
    
    commit c7c193ae7747b9cc6f33c715cfd6d832314cc382
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 18:29:04 2024 +0100
    
        Fix bugs
    
    commit e67e1831269e56a87efd5bab788f437e0fc50bce
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 17:19:02 2024 +0000
    
        Fix bug
    
    commit 91c0f8b17a7524dc624b937606b12aca7e7e92fd
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 17:15:47 2024 +0000
    
        Fix citation
    
    commit 6d9612303db1187b4c6f52f073009c57a17b9742
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 17:05:34 2024 +0000
    
        Add tests
    
    commit c2531b3243d120e05f63c443b4f524aeb567b134
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 17:54:11 2024 +0100
    
        Fix XML
    
    commit a0a9b162b5aa733880b65622871915c3c3877888
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 16:20:50 2024 +0000
    
        Implement `voronoi_tessellation` tool
    
    commit 05b5849ccb5e97faa1662943c3c2d14d3e0799f5
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 16:02:43 2024 +0000
    
        Add `voronoi_tesselation` tool

---
+++ tools/voronoi_tessellation/voronoi_tessellation.py
@@ -0,0 +1,33 @@
+import argparse
+
+import numpy as np
+import scipy.ndimage as ndi
+import skimage.io
+from skimage.segmentation import watershed
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input')
+    parser.add_argument('output')
+    args = parser.parse_args()
+
+    im = skimage.io.imread(args.input)
+    im = im.squeeze()
+    assert im.ndim == 2
+
+    distances = np.full(im.shape, np.inf)
+    for label in np.unique(im):
+        if label == 0:
+            continue
+
+        label_distances = ndi.distance_transform_edt(im != label)
+        distances = np.min((distances, label_distances), axis=0)
+
+    result = watershed(
+        image=distances,
+        markers=im,
+    )
+
+    skimage.io.imsave(args.output, result)
