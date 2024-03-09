080ffe7c352c4ef655b4ae3133710c7c43d357ba
Leonid Kostrykin <void@evoid.de>
2024-03-09 20:57:13 +0100

    Replace `eval` implementation
    Squashed commit of the following:
    
    commit 78edfb24856b4b8997497125b1e686576fd6c91f
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 19:56:41 2024 +0000
    
        Add comments
    
    commit 8bc558aab657f75fe2d40a95d7c2baa1e21ff524
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 19:52:26 2024 +0000
    
        Fix bug
    
    commit b6e301cf33f97ba664fdec23936de28ee920433c
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 19:29:06 2024 +0000
    
        Add missing nodes
    
    commit ed2fc08e551af7442c1986bae5f70592989fce2a
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 18:35:35 2024 +0000
    
        Add AST-based implementation
    
    commit 5b9c872b31d79dd467fd7144c08bf1475254983a
    Merge: 1c8752e eda7adf
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 18:22:08 2024 +0000
    
        Merge branch 'add-image-math-tool/upstream' into add-image-math-tool/dev
    
    commit eda7adf09f3fa3888a53e0f876a01e53eb768522
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 16:57:51 2024 +0100
    
        Update tools/image_math/image_math.xml
    
        Co-authored-by: Björn Grüning <bjoern@gruenings.eu>
    
    commit 1c8752ec4cfcb0dbd2ec9fb69de6eaeca1c9a414
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 11:45:54 2024 +0000
    
        Fix tool
    
    commit 62e7fe07276e2909195b32f313e446358c26db5d
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 11:43:09 2024 +0000
    
        Add more tests
    
    commit 9b761158f8456c703009ecd2e0a6ad55bf12b316
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Sat Mar 9 07:58:26 2024 +0000
    
        Fix regex
    
    commit 3c91ce8ad1338b92ca11feec2efdadd7cbc1030d
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Fri Mar 8 21:47:23 2024 +0000
    
        Update help
    
    commit 79ceef3bc47e2cf4d6c1b28f878f54e60bee3297
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Fri Mar 8 22:47:09 2024 +0100
    
        Fix tests
    
    commit 6e762c7dc464a6b81741501aa48c8fd45009cc34
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Fri Mar 8 21:39:58 2024 +0000
    
        Add tests
    
    commit 181b0b90c1ef53025bc27fad92e7f596f3de777a
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Fri Mar 8 22:34:07 2024 +0100
    
        Add test results
    
    commit 490e713fb5f293f264f4760b67bcc55d47cf444a
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Fri Mar 8 21:32:50 2024 +0000
    
        Fix bug
    
    commit 8ca30a3cea9a1b55365ab90748c87be723495e19
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Fri Mar 8 21:24:08 2024 +0000
    
        Add tests
    
    commit 321e2f946e65956d7c436957101b3aeb3e84fb1e
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Fri Mar 8 21:22:01 2024 +0000
    
        Fix bug
    
    commit 561c3829a8b3a8b7eaf47949dcd54c4ea4191f42
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Fri Mar 8 21:18:25 2024 +0000
    
        Fix bug
    
    commit 8ab67a3d43f9a19a771ad638bf6ec965006ae3f8
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Fri Mar 8 21:15:55 2024 +0000
    
        Fix bug
    
    commit 856f343f72046d33095b217f4427d2b7baa61d3f
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Fri Mar 8 21:06:30 2024 +0000
    
        Fix tool
    
    commit 966aa1fae7dddaa777a276efec8deb0076000b2c
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Fri Mar 8 21:02:04 2024 +0000
    
        Fix tool
    
    commit ef715eff89127980bc6ed56fe044d689dc0c68dd
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Fri Mar 8 21:01:10 2024 +0000
    
        Update tests
    
    commit edc1b91fc5b3621cbe0d74c5423f6a70b774cf40
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Fri Mar 8 20:58:44 2024 +0000
    
        Add `image_math` tool

--- tools/image_math/image_math.py
+++ tools/image_math/image_math.py
@@ -1,6 +1,68 @@
 import argparse
+import ast
+import operator
 
+import numpy as np
 import skimage.io
+
+
+supported_operators = {
+    ast.Add: operator.add,
+    ast.Sub: operator.sub,
+    ast.Mult: operator.mul,
+    ast.Div: operator.truediv,
+    ast.FloorDiv: operator.floordiv,
+    ast.Pow: operator.pow,
+    ast.USub: operator.neg,
+}
+
+
+supported_functions = {
+    'sqrt': np.sqrt,
+    'abs': abs,
+}
+
+
+def eval_ast_node(node, inputs):
+    """
+    Evaluates a node of the syntax tree.
+    """
+
+    # Numeric constants evaluate to numeric values.
+    if isinstance(node, ast.Constant):
+        assert type(node.value) in (int, float)
+        return node.value
+
+    # Variables are looked up from the inputs and resolved.
+    if isinstance(node, ast.Name):
+        assert node.id in inputs.keys()
+        return inputs[node.id]
+
+    # Binary operators are evaluated based on the `supported_operators` dictionary.
+    if isinstance(node, ast.BinOp):
+        assert type(node.op) in supported_operators.keys(), node.op
+        op = supported_operators[type(node.op)]
+        return op(eval_ast_node(node.left, inputs), eval_ast_node(node.right, inputs))
+
+    # Unary operators are evaluated based on the `supported_operators` dictionary.
+    if isinstance(node, ast.UnaryOp):
+        assert type(node.op) in supported_operators.keys(), node.op
+        op = supported_operators[type(node.op)]
+        return op(eval_ast_node(node.operand, inputs))
+
+    # Function calls are evaluated based on the `supported_functions` dictionary.
+    if isinstance(node, ast.Call):
+        assert len(node.args) == 1 and len(node.keywords) == 0
+        assert node.func.id in supported_functions.keys(), node.func.id
+        func = supported_functions[node.func.id]
+        return func(eval_ast_node(node.args[0], inputs))
+
+    # The node is unsupported and could not be evaluated.
+    raise TypeError(f'Unsupported node type: "{node}"')
+
+
+def eval_expression(expr, inputs):
+    return eval_ast_node(ast.parse(expr, mode='eval').body, inputs)
 
 
 if __name__ == '__main__':
@@ -23,10 +85,6 @@ if __name__ == '__main__':
         else:
             assert im.shape == im_shape, 'Input images differ in size and/or number of channels.'
 
-    result = eval(
-        args.expression,
-        dict(),  # globals
-        inputs,  # locals
-    )
+    result = eval_expression(args.expression, inputs)
 
     skimage.io.imsave(args.output, result)
