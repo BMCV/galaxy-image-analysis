8b9f24cbfaf54f140705f0bf4b6732269bd401da
Leonid Kostrykin <void@evoid.de>
2024-03-11 18:00:10 +0100

    Update "Threshold image" tool (#109)
    Squashed commit of the following:
    
    commit bc594cf1ebbf308dfb6dd1e7428dc190b9ae5578
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Mon Mar 11 15:55:40 2024 +0000
    
        Update tool version
    
    commit 92657e4e2e3662e60acf69d9d4b4c7e57a2da89f
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Mon Mar 11 15:54:38 2024 +0000
    
        Update help
    
    commit f8e732a92a67b7286b4ea6deb6b514b0481bf382
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Mon Mar 11 15:51:41 2024 +0000
    
        Add help
    
    commit 61247668b2a746d1c41be70a5c00145021bddfe7
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Mon Mar 11 15:33:11 2024 +0000
    
        Replace `dark_bg` by `invert_output`
    
    commit a41c52f8053c023a3233196f84a502a9e58237ad
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Mon Mar 11 12:42:16 2024 +0000
    
        Update UI
    
    commit 044709da1558285af194d7c6486607d3b1b021de
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Mon Mar 11 12:00:05 2024 +0000
    
        Add help
    
    commit e5630a8bb4aeb91997eef401832508816769270c
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Mon Mar 11 12:59:42 2024 +0100
    
        Fix test output
    
    commit 108ab022828cfc21df82f252e57065a2e458973c
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Mon Mar 11 11:50:17 2024 +0000
    
        Add test
    
    commit 64060615a56b9831d9e38090a9beee8e0b63c641
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Mon Mar 11 11:46:15 2024 +0000
    
        Add `manual` thresholding method
    
    commit 0a897841c21d267dc015e90658c125024d3ba622
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Mon Mar 11 11:36:42 2024 +0000
    
        Fix issues
    
    commit 912977ea865d7c09bfb5caa3c0c92bfbb38809c9
    Author: Leonid Kostrykin <void@evoid.de>
    Date:   Mon Mar 11 11:27:51 2024 +0000
    
        Refactor auto_threshold.xml

--- tools/2d_auto_threshold/auto_threshold.py
+++ tools/2d_auto_threshold/auto_threshold.py
@@ -1,37 +1,38 @@
 """
-Copyright 2017-2022 Biomedical Computer Vision Group, Heidelberg University.
+Copyright 2017-2024 Biomedical Computer Vision Group, Heidelberg University.
 
 Distributed under the MIT license.
 See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
-
 """
 
 import argparse
 
+import numpy as np
 import skimage.filters
 import skimage.io
 import skimage.util
 import tifffile
 
-thOptions = {
-    'otsu': lambda img_raw, bz: skimage.filters.threshold_otsu(img_raw),
-    'li': lambda img_raw, bz: skimage.filters.threshold_li(img_raw),
-    'yen': lambda img_raw, bz: skimage.filters.threshold_yen(img_raw),
-    'isodata': lambda img_raw, bz: skimage.filters.threshold_isodata(img_raw),
+th_methods = {
+    'manual': lambda thres, **kwargs: thres,
 
-    'loc_gaussian': lambda img_raw, bz: skimage.filters.threshold_local(img_raw, bz, method='gaussian'),
-    'loc_median': lambda img_raw, bz: skimage.filters.threshold_local(img_raw, bz, method='median'),
-    'loc_mean': lambda img_raw, bz: skimage.filters.threshold_local(img_raw, bz, method='mean')
+    'otsu': lambda img_raw, **kwargs: skimage.filters.threshold_otsu(img_raw),
+    'li': lambda img_raw, **kwargs: skimage.filters.threshold_li(img_raw),
+    'yen': lambda img_raw, **kwargs: skimage.filters.threshold_yen(img_raw),
+    'isodata': lambda img_raw, **kwargs: skimage.filters.threshold_isodata(img_raw),
+
+    'loc_gaussian': lambda img_raw, bz, **kwargs: skimage.filters.threshold_local(img_raw, bz, method='gaussian'),
+    'loc_median': lambda img_raw, bz, **kwargs: skimage.filters.threshold_local(img_raw, bz, method='median'),
+    'loc_mean': lambda img_raw, bz, **kwargs: skimage.filters.threshold_local(img_raw, bz, method='mean')
 }
 
 
-def auto_thresholding(in_fn, out_fn, th_method, block_size=5, dark_bg=True):
+def do_thresholding(in_fn, out_fn, th_method, block_size=5, threshold=0, invert_output=False):
     img = skimage.io.imread(in_fn)
-    th = thOptions[th_method](img, block_size)
-    if dark_bg:
-        res = img > th
-    else:
-        res = img <= th
+    th = th_methods[th_method](img_raw=img, bz=block_size, thres=threshold)
+    res = img > th
+    if invert_output:
+        res = np.logical_not(res)
     tifffile.imwrite(out_fn, skimage.util.img_as_ubyte(res))
 
 
@@ -39,9 +40,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Automatic Image Thresholding')
     parser.add_argument('im_in', help='Path to the input image')
     parser.add_argument('im_out', help='Path to the output image (TIFF)')
-    parser.add_argument('th_method', choices=thOptions.keys(), help='Thresholding method')
+    parser.add_argument('th_method', choices=th_methods.keys(), help='Thresholding method')
     parser.add_argument('block_size', type=int, default=5, help='Odd size of pixel neighborhood for calculating the threshold')
-    parser.add_argument('dark_bg', default=True, type=bool, help='True if background is dark')
+    parser.add_argument('threshold', type=float, default=0, help='Manual thresholding value')
+    parser.add_argument('invert_output', default=False, type=bool, help='Values below/above the threshold are labeled with 0/255 if False, and with 255/0 otherwise')
     args = parser.parse_args()
 
-    auto_thresholding(args.im_in, args.im_out, args.th_method, args.block_size, args.dark_bg)
+    do_thresholding(args.im_in, args.im_out, args.th_method, args.block_size, args.threshold, args.invert_output)
