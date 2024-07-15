3ee4097b440530b3ec179cd5d16a2a8f7e117dc4
Riccardo Massei <118002526+rmassei@users.noreply.github.com>
2024-07-15 10:19:34 +0000

    all trailing white spaces in the python code removed

--- tools/background_removal/background_removal.py
+++ tools/background_removal/background_removal.py
@@ -25,21 +25,21 @@ def process_image(args):
     with warnings.catch_warnings():
         output_image = convert_image_to_format_of(output_image, image)
         skimage.io.imsave(args.output, output_image, plugin="tifffile")
-    
-def convert_image_to_format_of(image, format_image):  
-    """  
-    Convert the first image to the format of the second image.  
-    """  
-    if format_image.dtype == image.dtype:  
-        return image  
-    elif format_image.dtype == np.uint8:  
-        return skimage.util.img_as_ubyte(image)  
-    elif format_image.dtype == np.uint16:  
-        return skimage.util.img_as_uint(image)  
-    elif format_image.dtype == np.int16:  
-        return skimage.util.img_as_int(image)  
-    else:  
-        raise ValueError(f'Unsupported image data type: {format_image.dtype}')  
+
+def convert_image_to_format_of(image, format_image):
+    """
+    Convert the first image to the format of the second image.
+    """
+    if format_image.dtype == image.dtype:
+        return image
+    elif format_image.dtype == np.uint8:
+        return skimage.util.img_as_ubyte(image)
+    elif format_image.dtype == np.uint16:
+        return skimage.util.img_as_uint(image)
+    elif format_image.dtype == np.int16:
+        return skimage.util.img_as_int(image)
+    else:
+        raise ValueError(f'Unsupported image data type: {format_image.dtype}')
 
 
 def main():
