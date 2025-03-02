94471d238467345a29bcdec30ed9389fd14e8e1c
Leonid Kostrykin <void@evoid.de>
2025-03-02 19:21:04 +0100

    Add tests and fix bugs

--- tools/split_image/split.py
+++ tools/split_image/split.py
@@ -1,6 +1,7 @@
 import argparse
+import math
 
-import giatools
+import giatools.io
 import numpy as np
 import tifffile
 
@@ -14,11 +15,11 @@ def str_without_positions(s: str, positions: list[int]) -> str:
     return s
 
 
-parser = argparse.Parser()
+parser = argparse.ArgumentParser()
 parser.add_argument('input', type=str)
 parser.add_argument('axis', type=str)
 parser.add_argument('output', type=str)
-parser.add_argument('--squeeze', type=bool, action='store_true', default=False)
+parser.add_argument('--squeeze', action='store_true', default=False)
 args = parser.parse_args()
 
 # Validate and normalize input parameters
@@ -26,7 +27,7 @@ assert len(args.axis) == 1
 axis = args.axis.replace('S', 'C')
 
 # Read input image as TZYXC
-img_in = giatools.imread(args.input)
+img_in = giatools.io.imread(args.input)
 axes = 'TZYXC'
 
 # Determine the axis to split along
@@ -34,13 +35,19 @@ axis_pos = axes.index(axis)
 
 # Perform the splitting
 arr = np.moveaxis(img_in, axis_pos, 0)
+decimals = math.ceil(math.log10(1 + arr.shape[0]))
+output_filename_pattern = f'{args.output}/%0{decimals}d.tiff'
 for img_idx, img in enumerate(arr):
     img = np.moveaxis(img[None], 0, axis_pos)
 
     # Optionally, squeeze the image
-    s = [axis_pos for axis_pos in range(len(axes)) if img.shape[axis_pos] == 1 and axes[axis_pos] not in 'YX']
-    img = np.squeeze(img, axis=s)
-    img_axes = str_without_positions(axes, s)
+    if args.squeeze:
+        s = [axis_pos for axis_pos in range(len(axes)) if img.shape[axis_pos] == 1 and axes[axis_pos] not in 'YX']
+        img = np.squeeze(img, axis=tuple(s))
+        img_axes = str_without_positions(axes, s)
+    else:
+        img_axes = axes
 
     # Save the result
-    tifffile.imwrite('', img, metadata=dict(axes=img_axes))
+    filename = output_filename_pattern % (img_idx + 1)
+    tifffile.imwrite(filename, img, metadata=dict(axes=img_axes))
