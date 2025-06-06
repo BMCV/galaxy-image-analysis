1775e3d0a483d795b2ded27d453d9df5579c820e
Leonid Kostrykin <void@evoid.de>
2025-06-06 23:19:16 +0200

    Bump to giatools 0.4.1 and skimage 0.25.0

--- tools/2d_auto_threshold/auto_threshold.py
+++ tools/2d_auto_threshold/auto_threshold.py
@@ -7,11 +7,10 @@ See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
 
 import argparse
 
-import giatools.io
 import numpy as np
 import skimage.filters
 import skimage.util
-import tifffile
+from giatools.image import Image
 
 
 th_methods = {
@@ -28,23 +27,45 @@ th_methods = {
 }
 
 
-def do_thresholding(in_fn, out_fn, th_method, block_size, offset, threshold, invert_output=False):
-    img = giatools.io.imread(in_fn)
-    img = np.squeeze(img)
-    assert img.ndim == 2
+def do_thresholding(
+    input_filepath: str,
+    output_filepath: str,
+    th_method: str,
+    block_size: int,
+    offset: float,
+    threshold: float,
+    invert_output: bool = False,
+):
+    assert th_method in th_methods, f'Unknown method "{th_method}"'
 
-    th = offset + th_methods[th_method](img_raw=img, bz=block_size, thres=threshold)
-    res = img > th
+    # Load image
+    img_in = Image.read(input_filepath)
+
+    # Perform thresholding
+    threshold = offset + th_methods[th_method](
+        img_raw=img_in.data,
+        bz=block_size,
+        thres=threshold,
+    )
+    result = img_in.data > threshold
     if invert_output:
-        res = np.logical_not(res)
+        result = np.logical_not(result)
 
-    tifffile.imwrite(out_fn, skimage.util.img_as_ubyte(res))
+    # Write result
+    Image(
+        data=skimage.util.img_as_ubyte(result),
+        axes=img_in.axes,
+    ).normalize_axes_like(
+        img_in.original_axes,
+    ).write(
+        output_filepath,
+    )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Automatic image thresholding')
-    parser.add_argument('im_in', help='Path to the input image')
-    parser.add_argument('im_out', help='Path to the output image (uint8)')
+    parser.add_argument('input', type=str, help='Path to the input image')
+    parser.add_argument('output', type=str, help='Path to the output image (uint8)')
     parser.add_argument('th_method', choices=th_methods.keys(), help='Thresholding method')
     parser.add_argument('block_size', type=int, default=5, help='Odd size of pixel neighborhood for calculating the threshold')
     parser.add_argument('offset', type=float, default=0, help='Offset of automatically determined threshold value')
@@ -52,4 +73,12 @@ if __name__ == "__main__":
     parser.add_argument('--invert_output', default=False, action='store_true', help='Values below/above the threshold are labeled with 0/255 by default, and with 255/0 if this argument is used')
     args = parser.parse_args()
 
-    do_thresholding(args.im_in, args.im_out, args.th_method, args.block_size, args.offset, args.threshold, args.invert_output)
+    do_thresholding(
+        args.input,
+        args.output,
+        args.th_method,
+        args.block_size,
+        args.offset,
+        args.threshold,
+        args.invert_output,
+    )
