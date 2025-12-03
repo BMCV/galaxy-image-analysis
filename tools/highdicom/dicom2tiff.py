fd191330fd820bb316d7ef3a20205a0e8045f258
Leonid Kostrykin <void@evoid.de>
2025-12-04 00:31:16 +0100

    Add `normalize_label_maps` option

--- tools/highdicom/dicom2tiff.py
+++ tools/highdicom/dicom2tiff.py
@@ -1,10 +1,12 @@
 import argparse
 import pathlib
+import sys
 from typing import Literal, get_args
 
 import giatools
 import highdicom
 import numpy as np
+import pydicom
 
 
 FrameAxis = Literal['T', 'Z', 'Q']
@@ -14,11 +16,12 @@ frame_axes = get_args(FrameAxis)
 def dicom_to_tiff(
     dcm_filepath: pathlib.Path,
     tiff_filepath: pathlib.Path,
-    multiframe_type: FrameAxis,
+    multiframe_axis: FrameAxis,
+    normalize_label_maps: bool,
     config: dict,
 ):
-    assert multiframe_type in frame_axes, (
-        f'Not a valid axis for DICOM frames: "{multiframe_type}"'
+    assert multiframe_axis in frame_axes, (
+        f'Not a valid axis for DICOM frames: "{multiframe_axis}"'
     )
     dcm = highdicom.imread(dcm_filepath)
 
@@ -52,7 +55,7 @@ def dicom_to_tiff(
         # Construct OME-TIFF axes string
         axes = 'YX'
         if dcm.number_of_frames >= 1:
-            axes = f'{multiframe_type}{axes}'
+            axes = f'{multiframe_axis}{axes}'
         if len(axes) < arr.ndim:
             axes += 'C'
 
@@ -61,10 +64,51 @@ def dicom_to_tiff(
         arr = arr[0]
         axes = axes[1:]
 
+    # Normalize label maps
+    if dcm.SOPClassUID == pydicom.uid.SegmentationStorage:
+        print('DICOM dataset is a label map')
+        if normalize_label_maps:
+            arr = normalize_label_map(arr)
+
     # Write TIFF file
     print('Output TIFF shape:', arr.shape)
     print('Output TIFF axes:', axes)
-    giatools.Image(arr, axes).write(tiff_filepath)
+    giatools.Image(arr, axes).write(str(tiff_filepath))
+
+
+def normalize_label_map(arr: np.ndarray) -> np.ndarray:
+    labels = np.unique(arr)
+    num_labels = len(labels)
+
+    # Choose appropriate data type
+    if num_labels < 2:
+        print(f'Too few labels ({num_labels}), skipping normalization')
+        norm_dtype = None
+    elif num_labels <= 0xff:
+        norm_dtype = np.uint8
+        norm_max_label = 0xff
+    elif num_labels <= 0xffff:
+        norm_dtype = np.uint16
+        norm_max_label = 0xffff
+    else:
+        print(f'Too many labels ({num_labels}), skipping normalization')
+        norm_dtype = None
+
+    # Create normalized label map
+    if norm_dtype is not None:
+        norm_arr = np.zeros(arr.shape, norm_dtype)
+        for label_idx, label in enumerate(sorted(labels)):
+            norm_label = (norm_max_label * label_idx) // (len(labels) - 1)
+            cc = (arr == label)
+            norm_arr[cc] = norm_label
+
+        # Verify that the normalization was loss-less, and return
+        # (normalization should never fail, but better check)
+        if len(np.unique(norm_arr)) != num_labels:
+            print('Label map normalization failed', file=sys.stderr)
+            return arr  #  this should never happen in practice, but better check
+        else:
+            return norm_arr
 
 
 if __name__ == '__main__':
@@ -73,15 +117,17 @@ if __name__ == '__main__':
     parser.add_argument('tiff', type=pathlib.Path)
     parser.add_argument('multiframe_axis', type=str, help='OME-TIFF axis to be used for the frames of a multi-frame DICOM file')
     parser.add_argument('dtype', type=str)
-    parser.add_argument('--apply_voi_transform', type=bool, default=False, action='store_true')
+    parser.add_argument('--normalize_label_maps', default=False, action='store_true')
+    parser.add_argument('--apply_voi_transform', default=False, action='store_true')
     args = parser.parse_args()
 
     dicom_to_tiff(
         args.dcm,
         args.tiff,
-        args.multiframe_axis,
-        dict(
-            dtype=args.dtype or None,  # use `None` instead of an empty string 
+        multiframe_axis=args.multiframe_axis,
+        normalize_label_maps=args.normalize_label_maps,
+        config=dict(
+            dtype=args.dtype or None,  # use `None` instead of an empty string
             apply_voi_transform=args.apply_voi_transform,
         ),
     )
