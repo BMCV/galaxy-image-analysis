38c29b248608811e2e7494602910cce46f51bee2
Leonid Kostrykin <void@evoid.de>
2025-12-03 22:21:44 +0100

    Fix handling of tiled mosaics and 3-D volumes

--- tools/highdicom/dicom2tiff.py
+++ tools/highdicom/dicom2tiff.py
@@ -15,28 +15,54 @@ def dicom_to_tiff(
     dcm_filepath: pathlib.Path,
     tiff_filepath: pathlib.Path,
     multiframe_type: FrameAxis,
+    config: dict,
 ):
     assert multiframe_type in frame_axes, (
         f'Not a valid axis for DICOM frames: "{multiframe_type}"'
     )
     dcm = highdicom.imread(dcm_filepath)
 
-    kwargs = dict()
-    kwargs['dtype'] = np.float32  # highdicom default is float64
-    kwargs['apply_voi_transform'] = False
+    # Set defaults
+    config = dict(config)
+    config.setdefault('dtype', np.float32)  # highdicom default is float64
+    config.setdefault('apply_voi_transform', False)
 
-    # Load frames (DICOM uses 1-based indexing)
-    arr = dcm.get_frames(**kwargs)
+    # If the image is a tiled image, ...
+    if hasattr(dcm, 'TotalPixelMatrixRows') and hasattr(dcm, 'TotalPixelMatrixColumns'):
 
-    # Construct OME-TIFF axes string
-    axes = 'YX'
-    if dcm.number_of_frames >= 1:
-        axes = f'{multiframe_type}{axes}'
-    if len(axes) < arr.ndim:
-        axes += 'C'
+        # ...extract the top-level WSI by combining the frames (tiles) into a mosaic
+        arr = dcm.get_total_pixel_matrix(**config)
+        axes = 'YX' if arr.ndim == 2 else 'YXC'
+
+    # Check if the image is a 3-D volume. According to the docs [1], `highdicom.Image.get_volume_geometry`
+    # will succeed (i.e. return a non-None) if the data *is* a 3-D volume, or if it is a tiled mosaic.
+    # Hence, to distinguish tiled images from 3-D volumes, we use the `elif` branching semantic.
+    #
+    # [1] https://highdicom.readthedocs.io/en/latest/package.html#highdicom.Image.get_volume_geometry
+    #
+    # So, if the image is a 3-D volume, ...
+    elif dcm.get_volume_geometry() is not None:
+
+        # ...extract the 3-D volume by joining the frames (z-slices)
+        arr = dcm.get_volume(**config).array
+        axes = 'ZYX' if arr.ndim == 3 else 'ZYXC'
+
+    # Otherwise, extract a raw stack of frames
+    else:
+        arr = dcm.get_frames(**kwargs)
+
+        # Construct OME-TIFF axes string
+        axes = 'YX'
+        if dcm.number_of_frames >= 1:
+            axes = f'{multiframe_type}{axes}'
+        if len(axes) < arr.ndim:
+            axes += 'C'
+
+    # Normalize singleton axes
     if axes[0] in frame_axes and arr.shape[0] == 1:
         arr = arr[0]
         axes = axes[1:]
+
     print('axes:', axes)
     print('shape:', arr.shape)
 
@@ -55,4 +81,5 @@ if __name__ == '__main__':
         args.dcm,
         args.tiff,
         args.multiframe_axis,
+        dict(),
     )
