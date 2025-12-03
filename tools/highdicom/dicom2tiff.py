d19cd8b515360354dabfbc1dbf07322742627178
Leonid Kostrykin <void@evoid.de>
2025-12-03 23:09:28 +0100

    Implement dicom2tiff.xml

--- tools/highdicom/dicom2tiff.py
+++ tools/highdicom/dicom2tiff.py
@@ -22,13 +22,9 @@ def dicom_to_tiff(
     )
     dcm = highdicom.imread(dcm_filepath)
 
-    # Set defaults
-    config = dict(config)
-    config.setdefault('dtype', np.float32)  # highdicom default is float64
-    config.setdefault('apply_voi_transform', False)
-
     # If the image is a tiled image, ...
-    if hasattr(dcm, 'TotalPixelMatrixRows') and hasattr(dcm, 'TotalPixelMatrixColumns'):
+    if dcm.is_tiled:
+        print('DICOM dataset is a tiled multi-frame image')
 
         # ...extract the top-level WSI by combining the frames (tiles) into a mosaic
         arr = dcm.get_total_pixel_matrix(**config)
@@ -42,6 +38,7 @@ def dicom_to_tiff(
     #
     # So, if the image is a 3-D volume, ...
     elif dcm.get_volume_geometry() is not None:
+        print('DICOM dataset is a 3-D volume')
 
         # ...extract the 3-D volume by joining the frames (z-slices)
         arr = dcm.get_volume(**config).array
@@ -49,6 +46,7 @@ def dicom_to_tiff(
 
     # Otherwise, extract a raw stack of frames
     else:
+        print('DICOM dataset is an unknown multi-frame image')
         arr = dcm.get_frames(**kwargs)
 
         # Construct OME-TIFF axes string
@@ -63,10 +61,9 @@ def dicom_to_tiff(
         arr = arr[0]
         axes = axes[1:]
 
-    print('axes:', axes)
-    print('shape:', arr.shape)
-
     # Write TIFF file
+    print('Output TIFF shape:', arr.shape)
+    print('Output TIFF axes:', axes)
     giatools.Image(arr, axes).write(tiff_filepath)
 
 
@@ -75,11 +72,16 @@ if __name__ == '__main__':
     parser.add_argument('dcm', type=pathlib.Path)
     parser.add_argument('tiff', type=pathlib.Path)
     parser.add_argument('multiframe_axis', type=str, help='OME-TIFF axis to be used for the frames of a multi-frame DICOM file')
+    parser.add_argument('dtype', type=str)
+    parser.add_argument('--apply_voi_transform', type=bool, default=False, action='store_true')
     args = parser.parse_args()
 
     dicom_to_tiff(
         args.dcm,
         args.tiff,
         args.multiframe_axis,
-        dict(),
+        dict(
+            dtype=args.dtype or None,  # use `None` instead of an empty string 
+            apply_voi_transform=args.apply_voi_transform,
+        ),
     )
