95bda12d9e2e3807676aae065c39bc03e24c5e0d
Leonid Kostrykin <void@evoid.de>
2025-12-09 19:46:16 +0100

    Add support for 3-D, multi-channel, multi-frame images

--- tools/2d_simple_filter/filter.py
+++ tools/2d_simple_filter/filter.py
@@ -1,28 +1,95 @@
 import argparse
+import json
+from typing import Any
 
 import giatools
+import numpy as np
 import scipy.ndimage as ndi
 from skimage.morphology import disk
 
 
 filters = {
-    'gaussian': lambda im, sigma: ndi.gaussian_filter(im, sigma),
-    'median': lambda im, radius: ndi.median_filter(im, footprint=disk(radius)),
-    'prewitt_h': lambda im, *args: ndi.prewitt(im, axis=1),
-    'prewitt_v': lambda im, *args: ndi.prewitt(im, axis=0),
-    'sobel_h': lambda im, *args: ndi.sobel(im, axis=1),
-    'sobel_v': lambda im, *args: ndi.sobel(im, axis=0),
+    'gaussian': lambda img, sigma: (
+        apply_2d_filter(ndi.gaussian_filter, img, sigma=sigma)
+    ),
+    'median': lambda img, radius: (
+        apply_2d_filter(ndi.median_filter, img, footprint=disk(radius))
+    ),
+    'prewitt_h': lambda img: (
+        apply_2d_filter(ndi.prewitt, img, axis=1)
+    ),
+    'prewitt_v': lambda img: (
+        apply_2d_filter(ndi.prewitt, img, axis=0)
+    ),
+    'sobel_h': lambda img: (
+        apply_2d_filter(ndi.sobel, img, axis=1)
+    ),
+    'sobel_v': lambda img: (
+        apply_2d_filter(ndi.sobel, img, axis=0)
+    ),
 }
+
+
+def apply_2d_filter(
+    filter_impl: callable,
+    img: giatools.Image,
+    **kwargs: Any,
+) -> giatools.Image:
+    """
+    Apply the 2-D filter to the 2-D/3-D, potentially multi-frame and multi-channel image.
+    """
+    result_data = None
+    for qtc in np.ndindex(
+        img.data.shape[ 0],  # Q axis
+        img.data.shape[ 1],  # T axis
+        img.data.shape[ 2],  # Z axis
+        img.data.shape[-1],  # C axis
+    ):
+        sl = np.s_[*qtc[:3], ..., qtc[3]]
+        arr = img.data[sl]
+        assert arr.ndim == 2  # sanity check, should always be True
+
+        # Perform 2-D filtering
+        res = filter_impl(arr, **kwargs)
+        if result_data is None:
+            result_data = np.empty(img.data.shape, res.dtype)
+        result_data[sl] = res
+
+    # Return results
+    return giatools.Image(result_data, img.axes)
+
+
+def apply_filter(
+    input_filepath: str,
+    output_filepath: str,
+    filter_type: str,
+    **kwargs: Any,
+):
+    # Read the input image
+    img = giatools.Image.read(input_filepath)
+
+    # Perform filtering
+    print(f'Applying filter: "{filter_type}"')
+    filter_impl = filters[filter_type]
+    res = filter_impl(img, **kwargs).normalize_axes_like(img.original_axes)
+
+    # Write the result
+    res.write(output_filepath, backend='tifffile')
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('input', type=str, help='Input image filepath')
     parser.add_argument('output', type=str, help='Output image filepath (TIFF)')
-    parser.add_argument('filter', choices=filters.keys(), help='Filter to be used')
-    parser.add_argument('size', type=float, help='Size of the filter (e.g., radius, sigma)')
+    parser.add_argument('params', type=str)
     args = parser.parse_args()
 
-    # Read the input image, perform filtering, write output (preserve metadata)
-    img = giatools.Image.read(args.input)
-    img.data = filters[args.filter](img.data, args.size)
-    img.normalize_axes_like(img.original_axes).write(args.output, backend='tifffile')
+    # Read the config file
+    with open(args.params) as cfgf:
+        cfg = json.load(cfgf)
+
+    apply_filter(
+        args.input,
+        args.output,
+        **cfg,
+    )
