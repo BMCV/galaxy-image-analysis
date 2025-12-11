5590095b1d0f69b8787d4f2b935ca31d73592811
Leonid Kostrykin <void@evoid.de>
2025-12-11 22:54:53 +0100

    Bump giatools to 0.5.2 for OME-TIFF support and metadata handling

--- tools/concat_channels/concat_channels.py
+++ tools/concat_channels/concat_channels.py
@@ -1,12 +1,10 @@
 import argparse
+from typing import Any
 
 import giatools
 import numpy as np
 import skimage.io
 import skimage.util
-
-
-normalized_axes = 'QTZYXC'
 
 
 def concat_channels(
@@ -16,10 +14,11 @@ def concat_channels(
     preserve_values: bool,
 ):
     # Create list of arrays to be concatenated
-    images = []
+    images = list()
+    metadata = dict()
     for image_path in input_image_paths:
 
-        img = giatools.Image.read(image_path, normalize_axes=normalized_axes)
+        img = giatools.Image.read(image_path, normalize_axes=giatools.default_normalized_axes)
         arr = img.data
 
         # Preserve values: Convert to `float` dtype without changing the values
@@ -30,17 +29,71 @@ def concat_channels(
         else:
             arr = skimage.util.img_as_float(arr)
 
+        # Record the metadata
+        for metadata_key, metadata_value in img.metadata.items():
+            metadata.setdefault(metadata_key, list())
+            metadata[metadata_key].append(metadata_value)
+
+        # Record the image data
         images.append(arr)
 
+    # Determine consensual metadata
+    # TODO: Convert metadata of images with different units of measurement into a common unit
+    final_metadata = dict()
+    for metadata_key, metadata_values in metadata.items():
+        if (metadata_value := reduce_metadata(metadata_values)) is not None:
+            final_metadata[metadata_key] = metadata_value
+
     # Do the concatenation
-    axis_pos = normalized_axes.index(axis)
+    axis_pos = giatools.default_normalized_axes.index(axis)
     arr = np.concatenate(images, axis_pos)
-    res = giatools.Image(arr, normalized_axes)
+    res = giatools.Image(
+        data=arr,
+        axes=giatools.default_normalized_axes,
+        metadata=final_metadata,
+    )
 
     # Squeeze singleton axes and save
-    squeezed_axes = ''.join(np.array(list(res.axes))[np.array(arr.shape) > 1])
-    res = res.squeeze_like(squeezed_axes)
+    res = res.squeeze()
+    print('Output TIFF shape:', res.data.shape)
+    print('Output TIFF axes:', res.axes)
+    print('Output TIFF', metadata_to_str(final_metadata))
     res.write(output_image_path, backend='tifffile')
+
+
+def reduce_metadata(values: list[Any]) -> Any | None:
+    non_none_values = list(filter(lambda value: value is not None, values))
+
+    # Reduction is not possible if more than one type is involved (or none)
+    value_types = [type(value) for value in non_none_values]
+    if len(frozenset(value_types)) != 1:
+        return None
+    else:
+        value_type = value_types[0]
+
+    # For floating point types, reduce via arithmetic average
+    if np.issubdtype(value_type, float):
+        return np.mean(non_none_values)
+
+    # For integer types, reduce via the median
+    if np.issubdtype(value_type, int):
+        return np.median(non_none_values)
+
+    # For all other types, reduction is only possible if the values are identical
+    if len(frozenset(non_none_values)) == 1:
+        return non_none_values[0]
+    else:
+        return None
+
+
+def metadata_to_str(metadata: dict) -> str:
+    tokens = list()
+    for key in sorted(metadata.keys()):
+        value = metadata[key]
+        if isinstance(value, tuple):
+            value = '(' + ', '.join([f'{val}' for val in value]) + ')'
+        tokens.append(f'{key}: {value}')
+    return ', '.join(tokens)
 
 
 if __name__ == "__main__":
