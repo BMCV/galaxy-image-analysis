a4380667e09fb2e699ba380fba2409bbad8351b8
Leonid Kostrykin <void@evoid.de>
2025-12-12 10:06:25 +0100

    Add test for multiseries OME TIFF

--- tools/split_image/split.py
+++ tools/split_image/split.py
@@ -10,16 +10,22 @@ import tifffile
 
 class OutputWriter:
 
-    def __init__(self, dir_path: pathlib.Path, num_images: int, squeeze: bool):
-        decimals = math.ceil(math.log10(1 + arr.shape[0]))
+    def __init__(self, dir_path: pathlib.Path, num_images: int, squeeze: bool, verbose: bool):
+        print(f'Writing {num_images} image(s)')
+        decimals = math.ceil(math.log10(1 + num_images))
         self.output_filepath_pattern = str(dir_path / f'%0{decimals}d.tiff')
         self.last_idx = 0
         self.squeeze = squeeze
+        self.verbose = verbose
 
     def write(self, img: giatools.Image):
         self.last_idx += 1
         if self.squeeze:
             img = img.squeeze()
+        if self.last_idx == 1 or self.verbose:
+            prefix = f'Output {self.last_idx}' if self.verbose else 'Output'
+            print(f'{prefix} axes:', img.axes)
+            print(f'{prefix} shape:', img.data.shape)
         img.write(self.output_filepath_pattern % self.last_idx)
 
 
@@ -39,8 +45,10 @@ if __name__ == '__main__':
         try:
             tiff = tifffile.TiffFile(args.input)
             num_tiff_series = len(tiff.series)
+            print(f'Found TIFF with {num_tiff_series} series')
         except tifffile.TiffFileError:
             num_tiff_series = 0  # not a TIFF file
+            print('Not a TIFF file')
 
         # If the file is a multi-series TIFF, extract the individual series
         # (for consistency, also accept only a single series if squeezing is requested)
@@ -49,10 +57,13 @@ if __name__ == '__main__':
                 dir_path=args.output,
                 num_images=num_tiff_series,
                 squeeze=args.squeeze,
+                verbose=True,
             )
             for series in range(num_tiff_series):
-                img = giatools.Image.read(args.input, series=series, backend='tifffile')
-                output.write(img)
+                img = giatools.Image.read(args.input, series=series)
+                output.write(
+                    img.squeeze_like(img.original_axes),
+                )
 
         # Otherwise, there is nothing to be split (or squeeze)
         # (the input is either a single-series TIFF or not a TIFF at all)
@@ -60,7 +71,14 @@ if __name__ == '__main__':
             os.symlink(args.input, args.output / '1.tiff')
         else:  # input is not a TIFF, conversion needed
             img = giatools.Image.read(args.input)
-            img.write(args.output / '1.tiff')
+            OutputWriter(
+                dir_path=args.output,
+                num_images=1,
+                squeeze=args.squeeze,
+                verbose=True,
+            ).write(
+                img.squeeze_like(img.original_axes),
+            )
 
     # If splitting along an image axes...
     else:
@@ -70,6 +88,8 @@ if __name__ == '__main__':
 
         # Read input image with normalized axes
         img_in = giatools.Image.read(args.input)
+        print('Input image axes:', img_in.original_axes)
+        print('Input image shape:', img_in.squeeze_like(img_in.original_axes).data.shape)
 
         # Determine the axis to split along
         axis_pos = img_in.axes.index(axis)
@@ -80,8 +100,9 @@ if __name__ == '__main__':
             dir_path=args.output,
             num_images=arr.shape[0],
             squeeze=args.squeeze,
+            verbose=False,
         )
-        for img in arr:
+        for img_idx, img in enumerate(arr):
             img = np.moveaxis(img[None], 0, axis_pos)
 
             # Construct the output image, remove axes added by normalization
@@ -93,5 +114,5 @@ if __name__ == '__main__':
                 img_in.original_axes,
             )
 
-            # Save the result
+            # Save the result (write stdout during first iteration)
             output.write(img_out)
