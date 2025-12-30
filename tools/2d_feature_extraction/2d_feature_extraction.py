35c97e562f77a6a919f6b636b89e66e704d993fc
Leonid Kostrykin <void@evoid.de>
2025-12-30 11:33:23 +0100

    Employ giatools 0.7.2 workaround, refactor tests

--- tools/2d_feature_extraction/2d_feature_extraction.py
+++ tools/2d_feature_extraction/2d_feature_extraction.py
@@ -3,19 +3,21 @@ import numpy as np
 import pandas as pd
 import skimage.measure
 
+from giatools__0_7_2__cli__ToolBaseplate import parse_args  # noqa: I202
+
 
 if __name__ == '__main__':
     tool = giatools.ToolBaseplate()
     tool.add_input_image('labels')
     tool.add_input_image('intensities', required=False)
     tool.parser.add_argument('--output', type=str)
-    tool.parse_args()
+    parse_args(tool)  # TODO: Revert to `tool.parse_args` when 0.7.2 is on Conda
 
     # Validate the input image
     try:
         label_image = tool.args.input_images['labels']
         if any(label_image.shape[label_image.axes.index(axis)] > 1 for axis in label_image.axes if axis not in 'ZYX'):
-            raise ValueError(f'This tool is not applicable to images with {label_image.axes} axes.')
+            raise ValueError(f'This tool is not applicable to images with {label_image.original_axes} axes.')
 
         # Extract the image features
         for section in tool.run('ZYX'):  # the validation code above guarantees that we will have only a single iteration
---
+++ tools/2d_feature_extraction/test-data/input/README.md
@@ -0,0 +1,56 @@
+# Overview of the test images
+
+## Label maps
+
+### `input9.zarr`:
+
+- axes: `ZYX`
+- resolution: `(2, 100, 100)`
+- dtype: `bool`
+- binary image
+- metadata:
+  - resolution: `(1.0, 1.0)`
+  - z-spacing: `1.0`
+  - unit: `um`
+
+### `input11.tiff`
+
+- axes: `YX`
+- resolution: `(265, 329)`
+- dtype: `uint16`
+- binary image
+
+### `input12.png`
+
+- axes: `YX`
+- resolution: `(58, 64)`
+- dtype: `uint8`
+- labels: `0...24`
+
+## Intensity images
+
+### `input1_uint8.tiff`:
+
+- axes: `YX`
+- resolution: `(265, 329)`
+- dtype: `uint8`
+- metadata: none
+
+### `input3_uint16.tiff`:
+
+- axes: `YXC`
+- resolution: `(58, 64, 3)`
+- dtype: `uint16`
+- metadata:
+  - resolution: `(2.0, 1.0)`
+  - unit: `mm`
+
+### `input8_zyx.zarr`:
+
+- axes: `ZYX`
+- resolution: `(2, 100, 100)`
+- dtype: `float64`
+- metadata:
+  - resolution: `(1.0, 1.0)`
+  - z-spacing: `1.0`
+  - unit: `um`
