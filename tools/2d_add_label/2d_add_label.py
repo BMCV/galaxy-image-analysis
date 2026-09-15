cfd964764eb272d2018659ea158de3bfa14a89af
Leonid Kostrykin <void@evoid.de>
2026-09-15 13:37:29 +0200

    Fix implementation

--- tools/2d_add_label/2d_add_label.py
+++ tools/2d_add_label/2d_add_label.py
@@ -79,73 +79,85 @@ if __name__ == "__main__":
             raise ValueError(f'This tool is not applicable to images with {n_channels} channels.')
 
         # Extract the image features
-        for section in tool.run('XYC'):  # the validation code above guarantees that we will have only a single iteration
+        #for section in tool.run('XYC'):  # the validation code above guarantees that we will have only a single iteration
 
-            # Create RGBA8 working copy of the input image
-            img = get_rgba8_copy(section['input_image'].data)
+        # TODO: expose as tool parameters
+        fp_lower = 'min'
+        fp_upper = 'max'
+        label_text_color = '#000000ff'
+        label_background_color = '#ffffffff'
+        fontsize = 12
+        position_v = tool.args.params['position_v']
+        position_h = tool.args.params['position_h']
 
-            # TODO: expose as tool parameters
-            label_text_color = '#000000ff'
-            label_background_color = '#ffffffff'
-            fontsize = 12
-            position_v = tool.args.params['position_v']
-            position_h = tool.args.params['position_h']
+        # Create RGBA8 working copy of the input image
+        #img = get_rgba8_copy(section['input_image'].data, fp_lower, fp_upper)
+        img = get_rgba8_copy(
+            tool.args.input_images['input_image'].normalize_axes_like('XYC').data,
+            fp_lower,
+            fp_upper,
+        )
 
-            # Determine the label height in pixels
-            label_height = (fontsize / 72) * 100
+        # Determine the label height in pixels
+        label_height = (fontsize * 100) // 72
 
-            # Extend the image above/below, if required
-            if position_v in ('above', 'below'):
-                label_bg_color = np.multiply(255, matplotlib.colors.to_rgba(label_background_color)).astype(np.uint8)
-                label_bg = np.full((label_height, img.shape[1]), 1, dtype=np.uint8) * label_bg_color
-                blocks = (img, label_bg)
-                if position_v == 'above':
-                    blocks = blocks[::-1]
-                img = np.concatenate(blocks, axis=0)
-                label_background_color = '#00000000'
+        # Extend the image above/below, if required
+        if position_v in ('above', 'below'):
+            label_bg_color = np.multiply(255, matplotlib.colors.to_rgba(label_background_color)).astype(np.uint8)
+            label_bg = np.full((label_height, img.shape[1], 4), 1, dtype=np.uint8) * label_bg_color
+            blocks = (img, label_bg)
+            if position_v == 'above':
+                blocks = blocks[::-1]
+            img = np.concatenate(blocks, axis=0)
+            label_background_color = '#00000000'
 
-            # Determine the vertical text alignment and coordinate
-            match position_v:
-                case 'above':
-                    position_v = 'top'
-                    text_y = 0
-                case 'below':
-                    position_v = 'bottom'
-                    text_y = img.shape[0] + label_height - 1
-                case 'top':
-                    text_y = 0
-                case 'bottom':
-                    text_y = img.shape[0] - 1
+        # Determine the vertical text alignment and coordinate
+        match position_v:
+            case 'above':
+                position_v = 'top'
+                text_y = 0
+            case 'below':
+                position_v = 'bottom'
+                text_y = img.shape[0] + label_height - 1
+            case 'top':
+                text_y = 0
+            case 'bottom':
+                text_y = img.shape[0] - 1
 
-            # Determine the horizontal text coordinate
-            match position_h:
-                case 'left':
-                    text_x = 0
-                case 'right':
-                    text_x = img.shape[1] - 1
-                case 'center':
-                    text_x = (img.shape[1] - 1) / 2
+        # Determine the horizontal text coordinate
+        match position_h:
+            case 'left':
+                text_x = 0
+            case 'right':
+                text_x = img.shape[1] - 1
+            case 'center':
+                text_x = (img.shape[1] - 1) / 2
 
-            # Create figure
-            fig = plt.figure(figsize=np.divide(img.shape[:2][::-1], 100), dpi=100)
-            ax = fig.add_axes([0, 0, 1, 1])
-            ax.axis('off')
-            ax.imshow(img)
+        # Create figure
+        fig = plt.figure(figsize=np.divide(img.shape[:2][::-1], 100), dpi=100)
+        ax = fig.add_axes([0, 0, 1, 1])
+        ax.axis('off')
+        ax.imshow(img)
 
-            # Paint text
-            ax.text(
-                text_x,
-                text_y,
-                str(tool.args.params['label_text']),
-                color=label_text_color,
-                backgroundcolor=label_background_color,
-                ha=position_h,
-                va=position_v,
-            )
+        # Paint text
+        ax.text(
+            text_x,
+            text_y,
+            str(tool.args.params['label_text']),
+            color=label_text_color,
+            backgroundcolor=label_background_color,
+            ha=position_h,
+            va=position_v,
+        )
+        #fig.canvas.draw()
 
-            # Determine the result image
-            img = np.array(fig.canvas.renderer._renderer)
-            section['output_image'] = img
+        # Determine the result image
+        #img = np.array(fig.canvas.renderer._renderer)
+        #section['output_image'] = img
+
+        # Write the result image
+        fig.canvas.print_png(tool.args.raw_args.output_image)
 
     except ValueError as err:
-        exit(err.args[0])
+        #exit(err.args[0])
+        raise
